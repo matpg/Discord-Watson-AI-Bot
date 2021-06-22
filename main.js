75fe97b805054a4e35609f3d273fcba159a9256a
Matias Poblete <mpg@ibm.com>
2021-06-21 20:42:15 -0400

    Limpieza de Codigo

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 /* Watson Discord BOT
   First 3 lines, npm modules imports
+  Las primeras 3 lineas importando modulos
 */
 const { Client, MessageEmbed, Collection } = require('discord.js');
 const fetch = require('node-fetch');
@@ -10,7 +11,7 @@ require('dotenv').config()
 
 const client = new Client();
 
-//prefix for the commands
+//Prefijo para los comandos, asi se invocarán cada uno
 const prefix = '!'
 
 const trim = (str, max) => str.length > max ? `${str.slice(0, max - 3)}...` : str;
@@ -25,19 +26,21 @@ for(let file of commandFiles){
 }
 
 //Turn on the bot user
+// Encender el chatbot
 client.on('ready', () => {
   console.log(`💯 Watson AI Bot is online!`);
 });
 
-//👨‍💻💉🍔🍕🌭🍙🍱🍣💯❓💝🤖👾👁
 
 //Summary of all bot responses and actions
+//Aqui se recopilan todas los comandos, llamadas a las APIs y respuestas del chatbot.
 client.on('message', async msg => {
     if (!msg.content.startsWith(prefix) || msg.author.bot) return;
     const args = msg.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
     // list of commands after prefix
+    // Lista de comandos luego del prefijo '!'
     if (command === 'ping'){
       client.commands.get('ping').execute(msg, args);
 
@@ -50,13 +53,14 @@ client.on('message', async msg => {
 
     } else if (command === 'urban') {
       if (!args.length) { return msg.reply('You need to supply a search term!'); }
-      //Query has the search text, natural text, etc
+
       const query = querystring.stringify({ term: args.join(' ') });
       const { list } = await fetch(`https://api.urbandictionary.com/v0/define?${query}`).then(response => response.json());
       if (!list.length) { return msg.reply(`No results found for **${args.join(' ')}**.`); }
       const [answer] = list;
       console.log(answer);
       //Template of info
+      // Template para ordenar información
       const embed = new MessageEmbed()
         .setColor('#EFFF00')
         .setTitle(answer.word)
@@ -66,6 +70,7 @@ client.on('message', async msg => {
         .addField('Rating', `${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.`);
         msg.reply(embed);
 
+      //Comandos de Watson AI y llamadas a las APIs
     } else if (command === 'mood'){
       if (!args.length) { return msg.reply('You need to supply a valid mood sentence!'); }
       const mood_comm_text = args.join(' ');
@@ -73,13 +78,11 @@ client.on('message', async msg => {
 
     } else if (command === 'topic') {
       if (!args.length) { return msg.reply('You need to supply a URL!'); }
-      //const topic_comm_text = querystring.stringify({ term: args.join(' ') });
       const topic_comm_text = args[0]
       client.commands.get('Natural Language Understanding Concepts').execute(msg, topic_comm_text);
         
     } else if (command === 'related') {
       if (!args.length) { return msg.reply('You need to supply a URL!'); }
-      //const topic_comm_text = querystring.stringify({ term: args.join(' ') });
       const related_comm_text = args[0]
       client.commands.get('Natural Language Understanding Categories').execute(msg, related_comm_text);
         
